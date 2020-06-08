21ed303bafe4db489a0ea10e1dbe6185ea3c07ca
Glitch (vr-research-exit) <none>
2020-06-08 13:28:59 +0000

    moved shooting game to a seperate file and started to look at adding quest controller

--- README.md
+++ README.md
@@ -13,6 +13,10 @@ The project
 The project uses A-Frame as the project uses WebVR. This is for a number of reasons including
 accessability and being able to carry out the research remotely if I need to.
 
+### Files
+I will try a a number of different things in Aframe to see what works best for the project. The main 
+content as always is in index.html, I am keeping other tests to track ideas, they will be in the test.html files
+
 
 
 ### assets
--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+let log = null;
+window.onload = ()=> {
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+const ctlL = document.getElementById("ctlL");
+const ctlR = document.getElementById("ctlR");
+const txt = document.getElementById("txt");
+
+log = function (v) {
+  txt.setAttribute("value", v);
+}
+
+ctlL.addEventListener("ybuttondown", function(event) {
+  log("y button pressed!");
+});
+ctlR.addEventListener("abuttondown", function(event) {
+  log("a button pressed!");
+});
+ctlR.addEventListener("bbuttondown", function(event) {
+  log("b button pressed!");
+});
+  
+}
+
+AFRAME.registerComponent("x-button-listener", {
+  init: function() {
+
+    this.el.addEventListener("xbuttondown", function(evt) {
+      log("x button pressed!");
+    });
+  }
+});
